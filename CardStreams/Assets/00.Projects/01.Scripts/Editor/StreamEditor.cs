a0d7e33f3111ea0ce7463fafe88d977dfabee238
flaby <agape513513@naver.com>
2022-06-07 14:34:43 +0900

    바닥 타일과 정산 연출 동전 이미지 변경

--- CardStreams/Assets/00.Projects/01.Scripts/Editor/StreamEditor.cs
+++ CardStreams/Assets/00.Projects/01.Scripts/Editor/StreamEditor.cs
@@ -6,11 +6,6 @@ using UnityEditor;
 public class StreamEditor : EditorWindow
 {
     IntValue goldValue;
-    EventSO TurnStartEvent;
-    EventSO TurnEndEvent;
-    EventSO MoveStartEvent;
-    EventSO MoveEndEvent;
-    EventSO NextTurnEvent;
     EventSO GoldChangeEvnet;
     
     [MenuItem("Window/StreamEditor")]
@@ -21,34 +16,8 @@ public class StreamEditor : EditorWindow
     private void OnGUI()
     {
         goldValue = Resources.Load<IntValue>("Gold");
-        TurnStartEvent = Resources.Load<EventSO>("TurnStart");
-        TurnEndEvent = Resources.Load<EventSO>("TurnEnd");
-        MoveStartEvent = Resources.Load<EventSO>("MoveStart");
-        MoveEndEvent = Resources.Load<EventSO>("MoveEnd");
-        NextTurnEvent = Resources.Load<EventSO>("NextTurn");
         GoldChangeEvnet = Resources.Load<EventSO>("GoldChange");
 
-
-        if (GUILayout.Button("TurnStart"))
-        {
-            TurnStartEvent.Occurred();
-        }
-        if (GUILayout.Button("TurnEnd"))
-        {
-            TurnEndEvent.Occurred();
-        }
-        if (GUILayout.Button("MoveStart"))
-        {
-            MoveStartEvent.Occurred();
-        }
-        if (GUILayout.Button("MoveEnd"))
-        {
-            MoveEndEvent.Occurred();
-        }
-        if (GUILayout.Button("NextTurn"))
-        {
-            NextTurnEvent.Occurred();
-        }
 
         if(GUILayout.Button("Get Gold 100"))
         {
