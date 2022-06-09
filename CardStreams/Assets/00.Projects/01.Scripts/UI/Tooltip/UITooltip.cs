86194eb52454f80e2095337d594d6bee1620f5ad
flaby <agape513513@naver.com>
2022-06-09 15:05:43 +0900

    UITooltip 만들기

--- CardStreams/Assets/00.Projects/01.Scripts/UI/Tooltip/UITooltip.cs
+++ CardStreams/Assets/00.Projects/01.Scripts/UI/Tooltip/UITooltip.cs
@@ -2,73 +2,110 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UITooltip : MonoBehaviour
 {
     public static UITooltip Instance { get; private set; }
 
-    private Text infoText;
+    [SerializeField]
+    private Canvas canvas;
+    private RectTransform canvasRectTrm;
+    private CanvasScaler scaler;
+
+    private RectTransform rectTrm;
+
+    private TextMeshProUGUI textMeshPro;
+
+    private RectTransform backgroundRectTrm;
 
     private TooltipTimer tooltipTimer;
+
+    private bool isShow;
 
     private void Awake()
     {
         Instance = this;
 
-        infoText = transform.Find("InfoText").GetComponent<Text>();
+        rectTrm = GetComponent<RectTransform>();
+        textMeshPro = this.transform.Find("text").GetComponent<TextMeshProUGUI>();
+        backgroundRectTrm = transform.Find("background").GetComponent<RectTransform>();
+
+        canvasRectTrm = canvas.GetComponent<RectTransform>();
+        scaler = canvas.GetComponent<CanvasScaler>();
 
         Hide();
     }
 
     private void Update()
     {
-        // 툴팁 타이머 처리
-        if (tooltipTimer != null)
+        if(isShow)
         {
-            tooltipTimer.timer -= Time.deltaTime;
-            if (tooltipTimer.timer <= 0)
+            HandleFollowMouse();
+
+            // 툴팁 타이머 처리
+            if (tooltipTimer != null)
             {
-                Hide();
+                tooltipTimer.timer -= Time.deltaTime;
+                if (tooltipTimer.timer <= 0)
+                {
+                    Hide();
+                }
             }
         }
+
     }
 
-    //private void HandleFollowMouse()
-    //{
-    //    // 마우스 포지션을 따라다니는 ui
-    //    // 화면상의 우측, 하단을 지나면 배경이 위치가 조정된다
-    //    Vector2 anchoredPos = Input.mousePosition / canvasRectTrm.lossyScale.x;
+    private void HandleFollowMouse()
+    {
+        // 마우스 포지션을 따라다니는 ui
+        // 화면상의 우측, 하단을 지나면 배경이 위치가 조정된다
+        Vector2 anchoredPos = (Input.mousePosition);// / canvasRectTrm.lossyScale.x;    // 
+        //anchoredPos.y = anchoredPos.y - canvasRectTrm.rect.height;
+        // 가로 위치 보정
+        if (anchoredPos.x + backgroundRectTrm.rect.width > canvasRectTrm.rect.width)
+        {
+            anchoredPos.x = canvasRectTrm.rect.width - backgroundRectTrm.rect.width;
+        }
 
-    //    // 가로 위치 보정
-    //    if (anchoredPos.x + backgroundRectTrm.rect.width > canvasRectTrm.rect.width)
-    //    {
-    //        anchoredPos.x = canvasRectTrm.rect.width - backgroundRectTrm.rect.width;
-    //    }
+        // 세로 위치 보정
+        if (anchoredPos.y + backgroundRectTrm.rect.height > canvasRectTrm.rect.height)
+        {
+            anchoredPos.y = canvasRectTrm.rect.height - backgroundRectTrm.rect.height;
+        }
 
-    //    // 세로 위치 보정
-    //    if (anchoredPos.y + backgroundRectTrm.rect.height > canvasRectTrm.rect.height)
-    //    {
-    //        anchoredPos.y = canvasRectTrm.rect.height - backgroundRectTrm.rect.height;
-    //    }
+        rectTrm.anchoredPosition = anchoredPos;
+    }
 
-    //    rectTrm.anchoredPosition = anchoredPos;
-    //}
+    void SetText(string tooltipText)
+    {
 
-    public void Show(string tooltipText, Vector3 pos, TooltipTimer tooltipTimer = null)
+        textMeshPro.SetText(tooltipText);
+        textMeshPro.ForceMeshUpdate();
+
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+        Vector2 padding = new Vector2(7, 7);
+        backgroundRectTrm.sizeDelta = textSize + padding;
+    }
+
+    public void Show(string tooltipText, TooltipTimer tooltipTimer = null)
     {
         // 타이머 세팅
         this.tooltipTimer = tooltipTimer;
 
-        infoText.text = tooltipText;
-
-        transform.position = pos;
-
         gameObject.SetActive(true);
+        SetText(tooltipText);
+
+        HandleFollowMouse();
+
+        isShow = true;
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+
+        isShow = false;
     }
 
     public class TooltipTimer
