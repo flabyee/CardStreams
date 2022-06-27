91368e0c125971776ab889c4e2269864ee862aea
vndfjsl <28alsrb@gmail.com>
2022-06-27 21:58:15 +0900

    스타팅 만듬

---
+++ CardStreams/Assets/00.Projects/01.Scripts/UI/Panel/GameStartingPanel.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartingPanel : MonoBehaviour
+{
+    [SerializeField] SpecialCardSO[] startingCards;
+    [SerializeField] Button[] startingButtons;
+
+    [SerializeField] GameObject itemInfo;
+    [SerializeField] Image itemImage;
+    [SerializeField] TextMeshProUGUI itemNameText;
+    [SerializeField] TextMeshProUGUI itemExplainText;
+
+    private CanvasGroup _cg;
+
+    private void Awake()
+    {
+        _cg = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        _cg.alpha = 1;
+        _cg.blocksRaycasts = true;
+        _cg.interactable = true;
+
+        itemInfo.SetActive(false);
+
+        for (int i = 0; i < startingButtons.Length; i++)
+        {
+            // ÀÌ¹ÌÁö¹Ù²Ù±â
+            startingButtons[i].image.sprite = startingCards[i].sprite;
+        }
+    }
+
+    public void PressStarting(int startingIndex)
+    {
+        // Á¤ÇØÁø ½ºÆä¼ÈÄ«µå È¹µæ
+        GameManager.Instance.handleController.AddSpecial(startingCards[startingIndex].id);
+
+        gameObject.SetActive(false);
+    }
+
+    public void EnterStarting(int startingIndex)
+    {
+        if(itemInfo.activeSelf == false)
+        {
+            itemInfo.SetActive(true);
+        }
+
+        itemImage.sprite = startingCards[startingIndex].sprite;
+        itemNameText.text = startingCards[startingIndex].specialCardName;
+        itemExplainText.text = startingCards[startingIndex].tooltip;
+    }
+}
