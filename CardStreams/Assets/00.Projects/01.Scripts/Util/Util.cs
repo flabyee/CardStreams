0967e6ef4f0cef375e0fdcaa1926d627d54384f3
vndfjsl <28alsrb@gmail.com>
2022-08-15 15:55:30 +0900

    util delay코루틴 수정

--- CardStreams/Assets/00.Projects/01.Scripts/Util/Util.cs
+++ CardStreams/Assets/00.Projects/01.Scripts/Util/Util.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoDelay
+public class Util
 {
     public static IEnumerator DelayCoroutine(float delayTime, Action completeCallback)
     {
